8affaca91757f98a996f65348461a3551f2a0389
morgane <morgane@les-tilleuls.coop>
2019-04-09 11:50:11 +0200

    se connecter avec user en bdd

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,26 +5,14 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Doctrine\ORM\EntityRepository;
 
-class UserRepository implements UserLoaderInterface
+class UserRepository extends EntityRepository implements UserLoaderInterface
 {
-    /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
+    public function loadUserByUsername($username)
     {
-        $this->entityManager = $entityManager;
-    }
-
-    public function loadUserByUsername($username): ?User
-    {
-        return $this->entityManager->createQueryBuilder()
-            ->select('u')
-            ->from(User::class, 'u')
+        return $this->createQueryBuilder('u')
             ->where('u.username = :username OR u.email = :username')
             ->setParameter('username', $username)
             ->getQuery()
